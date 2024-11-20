f6239a3e2933b1d2359d8353c84f35ecad4a3457
Caio A. Andrade <132414130+amareloazedo@users.noreply.github.com>
2024-11-20 14:04:36 -0300

    Create if & else & else if.js

---
+++ condicionais/if & else & else if.js
@@ -0,0 +1,28 @@
+  // variável boolean
+  let itsOn = true;
+​
+// se a variável tiver o valor "true" o comando do if será executado.
+if (itsOn) {
+  console.log("Executou comando!");
+}
+​
+  let itsEmpty = false;
+  ​
+if (itsEmpty) {
+  console.log("Está vazio!");
+} 
+// caso o if não seja válido e executado, o comando de else será executado.
+else {
+  console.log("Está lotado!");
+}
+  
+  let number = 5;
+​
+if (number == 3) {
+  console.log("o número é 3!");
+} 
+// caso o if não seja válido e executado, 
+// o comando de else if será validado e executado.
+else if (number == 5) {
+  console.log("o número é 5!");
+}
