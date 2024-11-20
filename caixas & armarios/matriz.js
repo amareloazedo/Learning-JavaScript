20939a0dd40c62903792b4951e31b1cff8aa761d
Caio A. Andrade <132414130+amareloazedo@users.noreply.github.com>
2024-11-20 13:00:58 -0300

    Create matriz.js

---
+++ caixas & armarios/matriz.js
@@ -0,0 +1,18 @@
+// criando uma matriz
+let pokemonTeam = [
+    // linha 0   
+    ["Charmander", 'F', "Nível 5"], 
+    // linha 1 
+    ["Squirtle", 'M', "Nível 6"], 
+    // linha 2 
+    ["Bulbassauro", 'F', "Nível 4"],
+  ];
+
+// imprimindo os dados do vetor Charmander:
+
+// linha 0, coluna 0
+console.log(pokemonTeam[0][0]); 
+// linha 0, coluna 1
+console.log(pokemonTeam[0][1]);]
+// linha 0, coluna 2
+console.log(pokemonTeam[0][2]);
