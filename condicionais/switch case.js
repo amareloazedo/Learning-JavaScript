c6167136ce79ea79349124ab38e80b45a282c3ad
Caio A. Andrade <132414130+amareloazedo@users.noreply.github.com>
2024-11-21 19:55:38 -0300

    Create switch case.js

---
+++ condicionais/switch case.js
@@ -0,0 +1,23 @@
+
+let fruta = "banana";
+
+// estrutura de decisão. 
+switch (fruta) {
+	
+    // condicional aplicada dentro do switch.
+	case "laranja":
+    console.log("suco de laranja!");
+    // quebra o switch se o case for 		executado com sucesso!
+    break;
+    
+    case "banana": 
+	console.log("vitamina de banana!");
+    break;
+    
+    case "maçã": 
+    console.log("suco de maçã!");
+    break;
+    
+    default: 
+    console.log("suco genérico!");
+}
