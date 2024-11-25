b9de87fee6ab0ea198563a7c4a35ebf75f49196f
Caio A. Andrade <132414130+amareloazedo@users.noreply.github.com>
2024-11-25 14:32:19 -0300

    Create while & do while & for.js

---
+++ condicionais/while & do while & for.js
@@ -0,0 +1,25 @@
+
+// laço de repetição for:
+
+// declara a variável( let i = 0);
+// declara o limite do valor da variável(i < 4);
+// declara o incremento a variável por repetição(i++);
+
+for (let i = 0; i <4; i++) {
+	console.log(i);
+}
+
+// laço de repetição while:
+
+let contador = 0;
+
+while (contador < 3) {
+	console.log("olá " + contador);
+    contador++;
+}
+
+// laço de repetição do while: 
+
+do {
+	console.log("oi! " + );
+} while (num)
